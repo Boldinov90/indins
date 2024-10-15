dad9f4ed6b617767c534b4159ae58648add718fa
Boldinov90 <71153593+Boldinov90@users.noreply.github.com>
2024-10-15 20:43:04 +0300

    start test project

--- src/store/index.js
+++ src/store/index.js
@@ -5,14 +5,20 @@ export default createStore({
       data: []
    },
    getters: {
+      DATA(state) {
+          return state.data
+      }
    },
    mutations: {
-      GET_DATA(state, respoonse) {
-         
+      GET_DATA(state, response) {
+         state.tasks = response.data
       }
    },
    actions: {
+      async GET_DATA({ commit }) {
+         const response = await axios.get('http://localhost:3000/tasks')
+         commit('GET_DATA', response)
+         return response
+      },
    },
-   modules: {
-   }
 })
