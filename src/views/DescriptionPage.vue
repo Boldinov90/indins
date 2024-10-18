505d298911afe74d9a2032b61b596d891788b230
Boldinov90 <71153593+Boldinov90@users.noreply.github.com>
2024-10-18 23:24:34 +0300

    Save progress

--- src/views/DescriptionPage.vue
+++ src/views/DescriptionPage.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>
-    <h3>Техн</h3>
-  </div>
+   <div class="a">
+      <h3>Задание реализовано с помощью:</h3>
+      <ul>
+         <li>Vue CLI</li>
+         <li>VueX</li>
+         <li>Vue-Router</li>
+         <li>Axios</li>
+         <li>Json-Server</li>
+         <li>SASS</li>
+      </ul>
+   </div>
 </template>
+
+<style lang="scss" scoped>
+div {
+   font-family: Arial, sans-serif;
+   margin: 0;
+   padding: 20px;
+   ul {
+      list-style-type: none;
+      padding: 0;
+      li {
+         background: #fff;
+         margin: 10px 0;
+         padding: 15px;
+         border-radius: 5px;
+         box-shadow: 0 2px 5px rgba(0, 0, 0, 0.102);
+      }
+      li:hover {
+         background: #f0f0f0;
+      }
+   }
+}
+</style>
